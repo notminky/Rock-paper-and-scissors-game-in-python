b1c8ac3e129d1c6fce6f464b67d871add390daa1
Bassem <125456027+notminky@users.noreply.github.com>
2024-02-09 16:35:40 +0200

    uploaded code

---
+++ game.py
@@ -0,0 +1,34 @@
+import random
+def get_choices():
+    while True:
+        my_choice=input("pick: rock, paper, scissores" "\n"+ "you choose: ").lower()
+        options = ["rock","paper","scissores"]
+        if my_choice in options:
+            break
+        else:
+            print("try again")
+    computer_choice = random.choice(options)
+    choices= {"player":my_choice,"computer":computer_choice}
+    return choices
+def check_win(player,computer):
+    print(f"Player chose {player}, Computer chose {computer}")
+    if player==computer:
+        return "it's a tie"
+    elif player== "rock":
+        if computer == "scissores":
+            return "you win"
+        else:
+            return "you lose"
+    elif player== "paper":
+        if computer == "rock":
+            return "you win"
+        else:
+            return "you lose"
+    elif player== "scissores":
+        if computer == "paper":
+            return "you win"
+        else:
+            return "you lose"
+choices = get_choices()
+win = check_win(choices["player"], choices["computer"])
+print(win)
